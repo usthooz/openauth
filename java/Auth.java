4783a361508c7edec0b082c371cb944ac41e045c
swxctx <swxctx@gmail.com>
2019-01-09 01:01:38 +0800

    java 三方认证

---
+++ java/Auth.java
@@ -0,0 +1,12 @@
+package github.com.usthooz.openauth.java;
+
+import java.io.IOException;
+
+/**
+ * Auth
+ */
+public class Auth {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
