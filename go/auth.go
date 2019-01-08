fc333e3254eefbd99ad688724c941a98f2a629f6
swxctx <swxctx@gmail.com>
2019-01-08 14:24:46 +0800

    QQ授权认证

--- go/auth.go
+++ go/auth.go
@@ -1,5 +1,11 @@
 package openauth
 
+import (
+	"fmt"
+
+	"github.com/usthooz/gutil/http"
+)
+
 const (
 	// QqAuthApi QQ授权api
 	QqAuthApi = "https://graph.qq.com/oauth2.0/me?access_token=%s"
@@ -10,3 +16,20 @@ const (
 	// XiaomiAuthApi 小米授权api
 	XiaomiAuthApi = "https://open.account.xiaomi.com/user/openidV2?token=%s&clientId=%s"
 )
+
+// QqAuth
+func QqAuth(qqAccessToken, qqOpenId string) (bool, error) {
+	_, resp, err := xhttp.GetJSON("https://graph.qq.com/oauth2.0/me?access_token=" + qqAccessToken)
+	if err != nil {
+		return false, err
+	}
+	data := resp["openid"]
+	if data == nil {
+		return false, fmt.Errorf("QqAuth: qq_open_id is nil, %+v", resp)
+	}
+	openId := data.(string)
+	if qqOpenId != openId {
+		return false, fmt.Errorf("QqAuth: qq_open_id not match, %s != %s", qqOpenId, openId)
+	}
+	return true, nil
+}
