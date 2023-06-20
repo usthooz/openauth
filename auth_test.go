388121017feec9c91b22beb3d4a5cd2714a3b284
swxctx <swxctx@gmail.com>
2023-06-20 15:16:00 +0800

    add test

---
+++ auth_test.go
@@ -0,0 +1,31 @@
+package openauth
+
+import (
+	openauth "github.com/usthooz/openauth/google"
+	"testing"
+)
+
+func TestQqAuth(t *testing.T) {
+	success, err := QqAuth("", "")
+	t.Logf("Success: %v, Err: %v", success, err)
+}
+
+func TestSinaAuth(t *testing.T) {
+	success, err := SinaAuth("", "")
+	t.Logf("Success: %v, Err: %v", success, err)
+}
+
+func TestWxAuth(t *testing.T) {
+	success, err := WxAuth("", "")
+	t.Logf("Success: %v, Err: %v", success, err)
+}
+
+func TestXiaomiAuth(t *testing.T) {
+	success, err := XiaomiAuth("", "", "")
+	t.Logf("Success: %v, Err: %v", success, err)
+}
+
+func TestGoogleVerify(t *testing.T) {
+	result, err := openauth.GoogleVerify("", "")
+	t.Logf("Success: %v, Err: %v", result, err)
+}
